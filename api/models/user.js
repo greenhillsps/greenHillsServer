9557bf1cde9260d484fb52d9fc3e9616bef46070
green hills <greenhillsps.edu@gmail.com>
2019-03-21 22:08:35 +0500

    intil

---
+++ api/models/user.js
@@ -0,0 +1,97 @@
+const mongoose = require('mongoose');
+const bcrypt =require('bcrypt');
+const jwt=require('jsonwebtoken');
+const SALT_I=10;
+require('dotenv').config();
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 5,
+
+    },
+  
+    role:{
+        type:Number,
+        default:0,
+    },
+    token:{
+        type:String
+    },
+    active:{
+        type:Boolean,
+        default:true
+    }
+    
+
+})
+
+//this run before save and then next run the save function
+userSchema.pre('save',function(next){
+    var user=this;
+ if(user.isModified('password')){
+     bcrypt.genSalt(SALT_I,function(err,salt){
+
+        if(err){
+            next(err)
+        }else{
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err){
+                    next(err)
+                }else{
+                    user.password=hash;
+                    next();
+                }
+            })
+        }
+    })
+ }else{
+     next();
+ }
+    
+})
+
+
+//when login password incrypt
+userSchema.methods.comparePassword=function(candidatePassword,cb)//cb for call back
+{
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
+    })
+}
+
+//generate token when login
+userSchema.methods.generateToken=function(cb){
+var user=this;
+var token=jwt.sign(user._id.toHexString(),process.env.SECRET);
+user.token=token;
+user.save(function(err,user){
+    if(err) return cb(err);
+    cb(null,user)
+})
+}
+
+
+//token verifying 
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+
+    jwt.verify(token,process.env.SECRET,function(err,decode){
+        user.findOne({"_id":decode,"token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user) 
+        })
+    })
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User }
---
+++ index.js
@@ -0,0 +1,80 @@
+const express =require('express');
+const bodyParser =require('body-parser');
+const cookieParser= require('cookie-parser');
+
+const formidable=require('express-formidable');
+
+const app=express();
+const mongoose =require('mongoose');
+require('dotenv').config();
+
+
+const { User } =require('./api/models/user');
+
+//mongoose connections
+mongoose.Promise=global.Promise;
+mongoose.connect(process.env.DATABASE,(err=>{
+    if(err){
+        console.log(process.env.DATABASE)
+        console.log("mongodb connection failed!!!!")
+    }else{
+        console.log("mongodb connected success")
+    }
+}))
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+//regiseter
+app.post('/api/users/register',(req,res)=>{
+    const user=new User(req.body);
+    user.save((err,doc)=>{
+        if(err){
+            res.status(400).json({
+                success:false,
+                err
+            })
+        }else{
+            res.status(200).json({
+                success:true,
+                //userdata:doc
+            })
+        }
+    })
+})
+
+
+//login
+app.post('/api/users/login',(req,res)=>{
+    //find the email
+    //check password
+    //generate a token
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user)return res.status(400).json({loginSuccess:false,message:'Auth failed, email not found'})
+    
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch)return res.status(400).json({loginSuccess:false,message:'Wrong password'});
+    
+            //generate token
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.status(200).json({
+                    loginSuccess:true,
+                    token:user
+                })
+            })
+        })
+        
+        
+    
+    })
+    })
+
+
+
+const port=process.env.PORT||3002;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
