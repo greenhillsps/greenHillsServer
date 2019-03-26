0ab2b7e95e000931386163421e58dfa738f154c0
uqair <muqairali@gmail.com>
2019-03-26 18:07:27 +0500

    treacher modal

---
+++ api/models/teacher.js
@@ -0,0 +1,54 @@
+const mongoose =require('mongoose');
+
+const teacherSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    fatherName:{
+        type:String,
+        required:true
+    },
+    cnic:{
+        type:String,
+        required:true
+    },
+    phone:{
+    type:Number,
+    required:true
+    },
+    qualification:{
+        type:Array,
+        required:true
+    },
+    experience:{
+        type:String,
+    },
+    profilePic:{
+   type:String,
+    },
+    joiningDate:{
+        type:Date,
+        required:true
+    },
+    tuitionSalary:{
+        type:Number,
+        required:true
+    },
+    transportSalary:{
+        type:Number,
+        default:0
+    },
+    otherSalary:{
+        type:Number,
+        default:0
+    },
+    totalSalary:{
+        type:Number,
+        required:true
+    }
+})
+
+const Teacher=mongoose.model('Teacher',teacherSchema);
+
+module.exports=Teacher;
