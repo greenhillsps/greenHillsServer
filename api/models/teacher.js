904d0af22872cb6375a833a430fe55570f261385
uqair <muqairali@gmail.com>
2019-03-27 18:40:27 +0500

    update and get teacher

--- api/models/teacher.js
+++ api/models/teacher.js
@@ -3,7 +3,7 @@ const mongoose =require('mongoose');
 const teacherSchema=mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
     },
     fatherName:{
         type:String,
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cors=require('cors');
-const cloudinary=require('cloudinary');
-const formidable=require('express-formidable');
+const cors = require('cors');
+const cloudinary = require('cloudinary');
+const formidable = require('express-formidable');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 
 const { User } = require('./api/models/user');
-const Teacher=require('./api/models/teacher');
+const Teacher = require('./api/models/teacher');
 //mongoose connections
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE, (err => {
@@ -30,9 +30,9 @@ app.use(cors())
 //cloudinary config use for uploading iamges and videos
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 })
 
 
@@ -67,8 +67,8 @@ app.post('/api/users/login', (req, res) => {
             if (!isMatch) return res.status(400).json({ loginSuccess: false, message: 'Wrong password' });
 
             //generate token
-            user.generateToken((err,user)=>{
-                if(err) return res.status(400).send(err);
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
                 res.status(200).json(user)
             })
         })
@@ -78,52 +78,53 @@ app.post('/api/users/login', (req, res) => {
     })
 })
 
-    //get users
-    app.get('/api/users',(req,res)=>{
-    User.find({active:true}).exec((err,data)=>{
-      if(err)res.status(400).json(err)
+//get users
+app.get('/api/users', (req, res) => {
+    User.find({ active: true }).exec((err, data) => {
+        if (err) res.status(400).json(err)
         else res.status(200).json(data)
     })
-    })
+})
 
-    //blocked unblocked users
-    app.get('/api/users-blockUnblock/:id/:block',(req,res)=>{
-        User.findByIdAndUpdate({_id:req.params.id},{blocked:req.params.block}, {new:true},(err,user)=>{
-            if(err) res.status(400).json(err)
-                else res.status(200).json(user)
-        })
+//blocked unblocked users
+app.get('/api/users-blockUnblock/:id/:block', (req, res) => {
+    User.findByIdAndUpdate({ _id: req.params.id }, { blocked: req.params.block }, { new: true }, (err, user) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json(user)
     })
+})
 
-    //delete users
-    app.get('/api/user-delete/:id',(req,res)=>{
-        User.remove({_id:req.params.id},(err,user)=>{
-            if(err) res.status(400).json(err)
-                else res.status(200).json(user)
-        })
+//delete users
+app.get('/api/user-delete/:id', (req, res) => {
+    User.remove({ _id: req.params.id }, (err, user) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json(user)
     })
-    
-    // //get students
-    // app.get('/students/:name',(req,res)=>{
-    // User.find({active:true, name: new RegExp(req.params.name, "i")}).exec((err,res)=>{
-    //   if(err)res.status(400).json({
-    //       message:err
-    //   })
-    //     else res.status(200).json(res)
-    // })
-    // })
+})
+
+// //get students
+// app.get('/students/:name',(req,res)=>{
+// User.find({active:true, name: new RegExp(req.params.name, "i")}).exec((err,res)=>{
+//   if(err)res.status(400).json({
+//       message:err
+//   })
+//     else res.status(200).json(res)
+// })
+// })
 
 //update user
-app.put('/api/updateUser/:id',(req,res)=>{
-    User.findByIdAndUpdate({_id:req.params.id},{name:req.body.name,email:req.body.email,role:req.body.role,password:req.body.password},{new:true},(err,user)=>{
-        if(err) res.status(400).json(err)
-            else res.status(200).json(user)
+app.put('/api/updateUser/:id', (req, res) => {
+    User.findByIdAndUpdate({ _id: req.params.id }, { name: req.body.name, email: req.body.email, role: req.body.role, password: req.body.password }, { new: true }, (err, user) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json(user)
     })
 })
 
 
 //register teacher
-app.post('/api/registerTeacher',(req,res)=>{
-     const teacher = new Teacher(req.body);
+app.post('/api/teacher/registerTeacher', (req, res) => {
+    console.log(req.body)
+    const teacher = new Teacher(req.body);
     teacher.save((err, doc) => {
         if (err) {
             res.status(400).json({
@@ -133,27 +134,56 @@ app.post('/api/registerTeacher',(req,res)=>{
         } else {
             res.status(200).json({
                 success: true,
-                  data:doc
+                data: doc
             })
         }
     })
 })
 
-
-
-//posting images
-app.post('/api/uploadimage',formidable(),(req,res)=>{
-    cloudinary.uploader.upload(req.files.file.path,(result)=>{
-     console.log(result);
-     res.status(200).send({
-         public_id:result.public_id,
-         url:result.url
-     })
-    },{
-        public_id:`${Date.now()}`,
-        resource_type:'auto'
+//get teacher data
+app.get('/api/teacher', (req, res) => {
+    Teacher.find({ active: true }).exec((err, data) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json(data)
     })
 })
+
+//update teacher 
+app.put('/api/updateTeacher/:id', (req, res) => {
+    const { name,fatherName,cnic,phone,qualification,experience,tuitionSalary,transportSalary,otherSalary,totalSalary,profilePic,joiningDate }=req.body;
+    Teacher.findByIdAndUpdate({ _id: req.params.id }, {
+        name: name,
+        fatherName: fatherName,
+        cnic: cnic,
+        phone: phone,
+        qualification: qualification,
+        experience: experience,
+        joiningDate: joiningDate,
+        tuitionSalary: tuitionSalary,
+        transportSalary: transportSalary,
+        otherSalary: otherSalary,
+        totalSalary: totalSalary,
+        profilePic:profilePic
+    }, { new: true }, (err, data) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json(data)
+    })
+})
+
+//posting images
+app.post('/api/uploadimage', formidable(), (req, res) => {
+    cloudinary.uploader.upload(req.files.file.path, (result) => {
+        console.log(result);
+        res.status(200).send({
+            public_id: result.public_id,
+            url: result.url
+        })
+    }, {
+            public_id: `${Date.now()}`,
+            resource_type: 'auto'
+        })
+})
+
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
